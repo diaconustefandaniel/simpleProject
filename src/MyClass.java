d9298102fb774ad867453a9bcb4f19ca31dc7d15
Diaconu Stefan <diaconustefandaniel@gmail.com>
2015-03-27 10:50:30 +0200

    primul commit de pe branch

--- src/MyClass.java
+++ src/MyClass.java
@@ -4,6 +4,7 @@ public class MyClass {
 	public static void main(String[] args) {
 		System.out.println("Salut!!!");
 		System.out.println("Salut din nou!!");
+		System.out.println("Ma ajuta Ana?");
 
 	}
 
